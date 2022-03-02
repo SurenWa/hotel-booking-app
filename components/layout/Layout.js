e6d090df4b5725be65bc7c26acf4ebf524286691
unknown <cirrusminor2@gmail.com>
2022-03-02 20:00:44 +0545

    first commit

---
+++ components/layout/Layout.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Head from 'next/head'
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import Header from './Header'
+import Footer from './Footer'
+
+
+
+const Layout = ({ children, title = "Book best hotels for your holiday" }) => {
+    return (
+        <div>
+            <Head>
+                <title>{title}</title>
+                <meta charSet='utf-8'/>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            </Head>
+
+            <Header />
+            <ToastContainer />
+                {children}
+            <Footer />
+        </div>
+    )
+}
+
+export default Layout
---
+++ config/dbConnect.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const dbConnect = () => {
+    if (mongoose.connection.readyState >= 1 ) {
+        return
+    }
+
+    mongoose.connect(process.env.DB_LOCAL_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(con => console.log("DB CONNECTED SUCCESSFULLY"))
+}
+
+export default dbConnect          
